df8005e210e6b3a14ee14422f4e4bb6ff89dec12
Gonzalo Cepeda <gjcepeda9@gmail.com>
2022-03-28 16:53:31 -0300

    Creé archivos para tarea del M4-U1

---
+++ M4-U1/frontend/src/pages/NosotrosPage.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const NosotrosPage = (props) => {
+    return (
+        <section className="holder">
+            <div class="historia">
+                <h2>Historia</h2>
+                <p>Lorem</p>
+                <p>Lorem</p>
+            </div>
+            <h2>Staff</h2>
+            <div className="personas">
+                <div className="persona">
+                    <img src="images/nosotros/nosotros1.jpg" width="75" alt="Juan"/>
+                    <h5>Juan Gómez</h5>
+                    <h6>Gerente general</h6>
+                    <p>Lorem</p>
+                </div>
+                <div className="persona">
+                    <img src="images/nosotros/nosotros2.jpg" width="75" alt="Juan"/>
+                    <h5>Juan Gómez</h5>
+                    <h6>Gerente general</h6>
+                    <p>Lorem</p>
+                </div>
+                <div className="persona">
+                    <img src="images/nosotros/nosotros3.jpg" width="75" alt="Juan"/>
+                    <h5>Juan Gómez</h5>
+                    <h6>Gerente general</h6>
+                    <p>Lorem</p>
+                </div>
+                <div className="persona">
+                    <img src="images/nosotros/nosotros4.jpg" width="75" alt="Juan"/>
+                    <h5>Juan Gómez</h5>
+                    <h6>Gerente general</h6>
+                    <p>Lorem</p>
+                </div>
+                <div className="persona">
+                    <img src="images/nosotros/nosotros5.jpg" width="75" alt="Juan"/>
+                    <h5>Juan Gómez</h5>
+                    <h6>Gerente general</h6>
+                    <p>Lorem</p>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default NosotrosPage;
---
+++ M4-U1/frontend/src/pages/NovedadesPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NovedadesPage (props) {
+    return (
+        <section className="holder">
+            <h2>Novedades</h2>
+            <h3>Título</h3>
+            <h4>Subtítulo</h4>
+            <p>cuerpo</p>
+        </section>
+    );
+}
+
+export default NovedadesPage;
